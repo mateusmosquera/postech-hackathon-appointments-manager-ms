852fa5b0669268f1708d748858fe1592de1b0487
mateusmosquera <mateusmosquera@hotmail.com>
2024-07-26 23:31:07 -0300

    SQS producer and Listener

--- src/main/kotlin/br/com/fiap/appointments/infrastructure/web/messaging/SQSListener.kt
+++ src/main/kotlin/br/com/fiap/appointments/infrastructure/web/messaging/SQSListener.kt
@@ -1,15 +1,15 @@
 package br.com.fiap.appointments.infrastructure.web.messaging
-//
-//import br.com.fiap.appointments.domain.usecases.AppointmentsDomainUseCase
-//import org.springframework.jms.annotation.JmsListener
-//import org.springframework.stereotype.Component
-//
-//@Component
-//class SQSListener(private val appointmentsDomainUseCase: AppointmentsDomainUseCase) {
-//
-//    @JmsListener(destination = "\${integration.sqs.url.listener}")
-//    fun receive(message: String) {
-//
-//        appointmentsDomainUseCase.updateAppointment(message)
-//    }
-//}
+
+import br.com.fiap.appointments.domain.usecases.AppointmentsDomainUseCase
+import org.springframework.jms.annotation.JmsListener
+import org.springframework.stereotype.Component
+
+@Component
+class SQSListener(private val appointmentsDomainUseCase: AppointmentsDomainUseCase) {
+
+    @JmsListener(destination = "\${integration.sqs.url.listener}")
+    fun receive(message: String) {
+
+        appointmentsDomainUseCase.updateAppointment(message)
+    }
+}
